294efbaa50cc321dead43920ddba76805265576b
ADMIN <ADMIN@LAB1-MAY09>
2024-03-04 08:08:31 +0700

    Add project files.

---
+++ LTCB_Switch.cpp
@@ -0,0 +1,31 @@
+#include  <iostream>
+using namespace std;
+int main() {
+	int ngay = 4;
+	switch (ngay) {
+	case 1:
+		cout << "thu hai";
+		break;
+	case 2:
+		cout << " thu ba";
+		break;
+	case 3:
+		cout << "thu tu";
+		break;
+	case 4:
+		cout << " thu nam";
+		break;
+	case 5:
+		cout << "thu sau";
+		break;
+	case 6:
+		cout << " thu sau";
+		break;
+	case 7:
+		cout << "thu bay";
+		break;
+	case 8:
+		cout << "chu nhat";
+		break;
+	}
+}
